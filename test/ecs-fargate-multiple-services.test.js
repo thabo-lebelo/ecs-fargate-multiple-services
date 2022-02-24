953e164bc366e2a3b9ad15ce0a61d4bb55b3e91a
Thabo Lebelo <thabolebelo@icloud.com>
2022-02-24 19:24:44 +0200

    Multiple Services ECS Deployment With The AWS CDK

---
+++ test/ecs-fargate-multiple-services.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const EcsFargateMultipleServices = require('../lib/ecs-fargate-multiple-services-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-fargate-multiple-services-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new EcsFargateMultipleServices.EcsFargateMultipleServicesStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
